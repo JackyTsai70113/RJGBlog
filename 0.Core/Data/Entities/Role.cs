76683fd8876960b1a6f707ec075fbc3dacf6e0fb
dick355140 <dick355140@live.com>
2021-01-03 19:24:36 +0800

    layout調整 新增Area 刪除identity相關的東西 新增Table User Role RoleUser

---
+++ 0.Core/Data/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public partial class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Auth { get; set; }
+    }
+}
--- 3.DAL/Data/RJGDbContext.cs
+++ 3.DAL/Data/RJGDbContext.cs
@@ -16,188 +16,23 @@ namespace DAL.Data
         {
         }
 
-        public virtual DbSet<Aspnetroleclaims> Aspnetroleclaims { get; set; }
-        public virtual DbSet<Aspnetroles> Aspnetroles { get; set; }
-        public virtual DbSet<Aspnetuserclaims> Aspnetuserclaims { get; set; }
-        public virtual DbSet<Aspnetuserlogins> Aspnetuserlogins { get; set; }
-        public virtual DbSet<Aspnetuserroles> Aspnetuserroles { get; set; }
-        public virtual DbSet<Aspnetusers> Aspnetusers { get; set; }
-        public virtual DbSet<Aspnetusertokens> Aspnetusertokens { get; set; }
         public virtual DbSet<Efmigrationshistory> Efmigrationshistory { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<Roleuser> Roleuser { get; set; }
         public virtual DbSet<Test> Test { get; set; }
+        public virtual DbSet<User> User { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=127.0.0.1;port=3307;user=root;password=password;database=db");
+                optionsBuilder.UseMySql("server=127.0.0.1;port=3307;user id=root;password=password;database=db", x => x.ServerVersion("8.0.22-mysql"));
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Aspnetroleclaims>(entity =>
-            {
-                entity.ToTable("aspnetroleclaims");
-
-                entity.HasIndex(e => e.RoleId)
-                    .HasName("IX_AspNetRoleClaims_RoleId");
-
-                entity.Property(e => e.ClaimType).HasColumnType("longtext");
-
-                entity.Property(e => e.ClaimValue).HasColumnType("longtext");
-
-                entity.Property(e => e.RoleId)
-                    .IsRequired()
-                    .HasMaxLength(255);
-
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.Aspnetroleclaims)
-                    .HasForeignKey(d => d.RoleId)
-                    .HasConstraintName("FK_AspNetRoleClaims_AspNetRoles_RoleId");
-            });
-
-            modelBuilder.Entity<Aspnetroles>(entity =>
-            {
-                entity.ToTable("aspnetroles");
-
-                entity.HasIndex(e => e.NormalizedName)
-                    .HasName("RoleNameIndex")
-                    .IsUnique();
-
-                entity.Property(e => e.Id).HasMaxLength(255);
-
-                entity.Property(e => e.ConcurrencyStamp).HasColumnType("longtext");
-
-                entity.Property(e => e.Name).HasMaxLength(256);
-
-                entity.Property(e => e.NormalizedName).HasMaxLength(256);
-            });
-
-            modelBuilder.Entity<Aspnetuserclaims>(entity =>
-            {
-                entity.ToTable("aspnetuserclaims");
-
-                entity.HasIndex(e => e.UserId)
-                    .HasName("IX_AspNetUserClaims_UserId");
-
-                entity.Property(e => e.ClaimType).HasColumnType("longtext");
-
-                entity.Property(e => e.ClaimValue).HasColumnType("longtext");
-
-                entity.Property(e => e.UserId)
-                    .IsRequired()
-                    .HasMaxLength(255);
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Aspnetuserclaims)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserClaims_AspNetUsers_UserId");
-            });
-
-            modelBuilder.Entity<Aspnetuserlogins>(entity =>
-            {
-                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey })
-                    .HasName("PRIMARY");
-
-                entity.ToTable("aspnetuserlogins");
-
-                entity.HasIndex(e => e.UserId)
-                    .HasName("IX_AspNetUserLogins_UserId");
-
-                entity.Property(e => e.LoginProvider).HasMaxLength(128);
-
-                entity.Property(e => e.ProviderKey).HasMaxLength(128);
-
-                entity.Property(e => e.ProviderDisplayName).HasColumnType("longtext");
-
-                entity.Property(e => e.UserId)
-                    .IsRequired()
-                    .HasMaxLength(255);
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Aspnetuserlogins)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserLogins_AspNetUsers_UserId");
-            });
-
-            modelBuilder.Entity<Aspnetuserroles>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.RoleId })
-                    .HasName("PRIMARY");
-
-                entity.ToTable("aspnetuserroles");
-
-                entity.HasIndex(e => e.RoleId)
-                    .HasName("IX_AspNetUserRoles_RoleId");
-
-                entity.Property(e => e.UserId).HasMaxLength(255);
-
-                entity.Property(e => e.RoleId).HasMaxLength(255);
-
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.Aspnetuserroles)
-                    .HasForeignKey(d => d.RoleId)
-                    .HasConstraintName("FK_AspNetUserRoles_AspNetRoles_RoleId");
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Aspnetuserroles)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserRoles_AspNetUsers_UserId");
-            });
-
-            modelBuilder.Entity<Aspnetusers>(entity =>
-            {
-                entity.ToTable("aspnetusers");
-
-                entity.HasIndex(e => e.NormalizedEmail)
-                    .HasName("EmailIndex");
-
-                entity.HasIndex(e => e.NormalizedUserName)
-                    .HasName("UserNameIndex")
-                    .IsUnique();
-
-                entity.Property(e => e.Id).HasMaxLength(255);
-
-                entity.Property(e => e.ConcurrencyStamp).HasColumnType("longtext");
-
-                entity.Property(e => e.Email).HasMaxLength(256);
-
-                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
-
-                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
-
-                entity.Property(e => e.PasswordHash).HasColumnType("longtext");
-
-                entity.Property(e => e.PhoneNumber).HasColumnType("longtext");
-
-                entity.Property(e => e.SecurityStamp).HasColumnType("longtext");
-
-                entity.Property(e => e.UserName).HasMaxLength(256);
-            });
-
-            modelBuilder.Entity<Aspnetusertokens>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name })
-                    .HasName("PRIMARY");
-
-                entity.ToTable("aspnetusertokens");
-
-                entity.Property(e => e.UserId).HasMaxLength(255);
-
-                entity.Property(e => e.LoginProvider).HasMaxLength(128);
-
-                entity.Property(e => e.Name).HasMaxLength(128);
-
-                entity.Property(e => e.Value).HasColumnType("longtext");
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Aspnetusertokens)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserTokens_AspNetUsers_UserId");
-            });
-
             modelBuilder.Entity<Efmigrationshistory>(entity =>
             {
                 entity.HasKey(e => e.MigrationId)
@@ -205,11 +40,51 @@ namespace DAL.Data
 
                 entity.ToTable("__efmigrationshistory");
 
-                entity.Property(e => e.MigrationId).HasMaxLength(95);
+                entity.Property(e => e.MigrationId)
+                    .HasColumnType("varchar(95)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
 
                 entity.Property(e => e.ProductVersion)
                     .IsRequired()
-                    .HasMaxLength(32);
+                    .HasColumnType("varchar(32)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("role");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Auth)
+                    .HasColumnType("varchar(250)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Name)
+                    .HasColumnType("varchar(10)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            modelBuilder.Entity<Roleuser>(entity =>
+            {
+                entity.HasKey(e => new { e.RoleId, e.UserAccount })
+                    .HasName("PRIMARY")
+                    .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+
+                entity.ToTable("roleuser");
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.UserAccount)
+                    .HasColumnType("varchar(20)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
             });
 
             modelBuilder.Entity<Test>(entity =>
@@ -220,11 +95,44 @@ namespace DAL.Data
                     .HasName("ID_UNIQUE")
                     .IsUnique();
 
-                entity.Property(e => e.Id).HasColumnName("ID");
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
 
                 entity.Property(e => e.Name)
                     .IsRequired()
-                    .HasMaxLength(50);
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Account)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("user");
+
+                entity.Property(e => e.Account)
+                    .HasColumnType("varchar(20)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.CreateTime).HasColumnType("datetime");
+
+                entity.Property(e => e.PassWord)
+                    .IsRequired()
+                    .HasColumnType("varchar(20)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.UpdateTime).HasColumnType("datetime");
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasColumnType("varchar(20)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
             });
 
             OnModelCreatingPartial(modelBuilder);
