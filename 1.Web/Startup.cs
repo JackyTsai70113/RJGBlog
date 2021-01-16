a16de60db5ca420bec9155d2516dd512205bcf1b
dick355140 <dick355140@live.com>
2021-01-16 14:24:58 +0800

    補上Area的Route設定

--- 1.Web/Startup.cs
+++ 1.Web/Startup.cs
@@ -64,9 +64,12 @@ namespace Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                                             name: "areas",
+                                             pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                                             name: "default",
+                                             pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
